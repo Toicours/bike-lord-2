fb5b9b3a0fa6928e20b04c8b0c1f8594e41b91b0
charles brotons <chbrotons@hotmail.com>
2021-11-15 18:03:24 +0100

    user controller, user model, view sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  regex_email = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
-  regex_phone_number = /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/ix
+  regex_email = /(\A$)|(^(http|https):\/\/[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z)/ix
+  regex_phone_number = /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/ix
   validates :email, uniqueness: { case_sensitive: false }, format: { with: regex_email }, presence: true
   validates :phone_number, format: { with: regex_phone_number }, uniqueness: { case_sensitive: false }, presence: true
   validates :address, length: { minimum: 5 }, presence: true
   validates :first_name, :last_name, presence: true
-  # has_many :rentals
-  # has_many :bikes, through: :rentals
+  has_many :rentals
+  has_many :bikes, through: :rentals
 
 end
