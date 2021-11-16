94a62bd0666e3ed115c6cf6f3024edd588fc0432
Mathieu Courtois <kourtois@protonmail.com>
2021-11-16 09:42:28 +0100

    fix routes (plural)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'bikes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bike, only: [:new, :create, :show] do
+  resources :bikes, only: [:new, :create, :show] do
     resources :rentals, only: [:new, :create]
   end
 end
