67dfad6cadaeba41d6055391d4d1d1acbbd73c37
antoine pecheux <ant.pecheux@gmail.com>
2021-11-16 13:00:03 +0100

    Seed without images done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@categories = ["Bike", "Scooter", "Moto"]
+@user1 = User.create!(email: "paul@gmail.com", is_owner: true, first_name: "Paul", last_name: "Predo", address: "5 rue des Flaques", phone_number: "O611223344", password: "123456")
+
+api_url = "https://bikeindex.org/api/v3/search"
+bikes = JSON.parse(URI.open(api_url).read)["bikes"]
+
+bikes.each do |bike|
+  if bike["description"].nil?
+    description = bike["title"]
+  else
+    description = bike["description"]
+  end
+  Bike.create(
+    name: bike["manufacturer_name"],
+    description: bike["title"],
+    category: @categories.sample,
+    available: true,
+    price: rand(0.100),
+    user: User.first
+  )
+end
