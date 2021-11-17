6b6a4d9573b30dd9124798605ccffd1b1e60982c
Mathieu Courtois <kourtois@protonmail.com>
2021-11-17 12:00:08 +0100

    adding routes to dashboard + implementing destroy in pundit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'bikes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bikes, only: [:new, :create, :show] do
+  resources :bikes, except: :index do
     resources :rentals, only: [:new, :create]
   end
   get "/dashboard", to: 'pages#dashboard'
